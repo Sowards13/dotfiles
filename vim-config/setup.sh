302ea6ee8f05b4b3da82f72465fb010416ba1dd9
Sowards13 <danesowards@gmail.com>
2026-02-20 22:47:26 -0500

    created and added script

---
+++ vim-config/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#  Paths
+REPO_VIMRC="$HOME/dotfiles/vim-config/.vimrc"
+HOME_VIMRC="$HOME/.vimrc"
+VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
+
+echo "--- Beginning Dotfiles Setup ---"
+
+#  Copy .vimrc from repo to home directory
+echo "Copying .vimrc from vim-config folder..."
+cp "$REPO_VIMRC" "$HOME_VIMRC"
+
+#  Install Vundle if it does not exist
+if [ ! -d "$VUNDLE_DIR" ]; then
+    echo "Vundle not found. Installing..."
+    git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
+else
+    echo "Vundle already installed. Skipping clone."
+fi
+
+# Install vim setup per .vimrc contents
+echo "Installing plugins..."
+vim +PluginInstall +qall
+
+echo "--- Completed ---"
