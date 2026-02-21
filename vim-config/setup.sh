4b5c2e88ff9e391262bf88eb039409368f7ecded
Sowards13 <danesowards@gmail.com>
2026-02-20 23:53:59 -0500

    removed uneeded periods

--- vim-config/setup.sh
+++ vim-config/setup.sh
@@ -8,19 +8,19 @@ VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
 echo "--- Beginning Dotfiles Setup ---"
 
 #  Copy .vimrc from repo to home directory
-echo "Copying .vimrc from vim-config folder..."
+echo "Copying .vimrc from vim-config folder"
 cp "$REPO_VIMRC" "$HOME_VIMRC"
 
 #  Install Vundle if it does not exist
 if [ ! -d "$VUNDLE_DIR" ]; then
-    echo "Vundle not found. Installing..."
+    echo "Vundle not found. Installing"
     git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
 else
     echo "Vundle already installed. Skipping clone."
 fi
 
 # Install vim setup per .vimrc contents
-echo "Installing plugins..."
+echo "Installing plugins"
 vim +PluginInstall +qall
 
 echo "--- Completed ---"
